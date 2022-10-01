778373821d4652b6bfa4ce9be706efc4cd82b9dd
AllyHussein <alihussein95@outlook.com>
2022-10-01 16:49:26 +0200

    gpt-3

---
+++ src/components/cta/Cta.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Cta.css"
+function Cta() {
+  return (
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access To Get Started</p>
+        <h3>Register Today & Start Exploring The Endless Possiblities</h3>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
+  )
+}
+
+export default Cta
---
+++ src/containers/possibility/Possiblity.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Possiblity.css"
+import possiblityImage from "../../assets/possibility.png"
+function Possiblity() {
+  return (
+    <div className='gpt3__possibility section__padding' id='possiblity'>
+      <div className='gpt3__possibility-image'>
+        <img src={possiblityImage} alt='possiblity' />
+      </div>
+      <div className='gpt3__possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The Possiblities Are Beyond Your Imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  )
+}
+
+export default Possiblity
